a4475f78214455608b7ae4b8da1deb1e3dd680ba
Galzai <galzaidenstein@gmail.com>
2019-04-23 19:04:37 +0300

    fixed keymap formatting again

--- main/keymap.c
+++ main/keymap.c
@@ -65,51 +65,47 @@ uint16_t default_slave_encoder_map[LAYERS][ENCODER_SIZE] = {
 #define XXXXXXX KC_NO
 
 // Each keymap is represented by an array, with an array that points to all the keymaps  by order
-uint16_t _QWERTY[MATRIX_ROWS][KEYMAP_COLS] = {
+	 uint16_t _QWERTY[MATRIX_ROWS][KEYMAP_COLS]={
 
-/* Qwerty
- * ,------------------------------------------------------------------------------------.
- * | Esc  |   Q  |   W  |   E   |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
- * |------+------+------+-------+------+-------------+------+------+------+------+------|
- * | Tab  |   A  |   S  |   D   |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+-------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C   |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
- * |------+------+------+-------+------+------+------+------+------+------+------+------|
- * | Ctrl | GUI  |  Alt |Default|Lower |Space |Space |Raise | Left | Down |  Up  |Right |
- * `------------------------------------------------------------------------------------'
- */
+			/* Qwerty
+			 * ,------------------------------------------------------------------------------------.
+			 * | Esc  |   Q  |   W  |   E   |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+			 * |------+------+------+-------+------+-------------+------+------+------+------+------|
+			 * | Tab  |   A  |   S  |   D   |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
+			 * |------+------+------+-------+------+------|------+------+------+------+------+------|
+			 * | Shift|   Z  |   X  |   C   |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+			 * |------+------+------+-------+------+------+------+------+------+------+------+------|
+			 * | Ctrl | GUI  |  Alt |Default|Lower |Space |Space |Raise | Left | Down |  Up  |Right |
+			 * `------------------------------------------------------------------------------------'
+			 */
 
-{ KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC },
-		{ KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN,
-				KC_QUOT }, { KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M,
-				KC_COMM, KC_DOT, KC_SLSH, KC_ENT }, { KC_LCTRL, KC_LGUI,
-				KC_LALT, DEFAULT, NUM_H, KC_SPC, KC_SPC, RAISE, KC_LEFT,
-				KC_DOWN, KC_UP, KC_RGHT }
+			  {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC },
+			  {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT },
+			  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT } ,
+			  {KC_LCTRL,KC_LGUI, KC_LALT, DEFAULT, NUM_H,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT }
 
-};
+	};
 
-uint16_t _NUM[MATRIX_ROWS][KEYMAP_COLS] = {
+	 uint16_t _NUM[MATRIX_ROWS][KEYMAP_COLS]={
 
-/* Nums
- * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
+			 /* Nums
+			  * ,-----------------------------------------------------------------------------------.
+			  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+			  * |------+------+------+------+------+-------------+------+------+------+------+------|
+			  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+			  * |------+------+------+------+------+------|------+------+------+------+------+------|
+			  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
+			  * |------+------+------+------+------+------+------+------+------+------+------+------|
+			  * |      |      |      |      |      |             |      |      |      |      |      |
+			  * `-----------------------------------------------------------------------------------'
+			  */
 
-{ KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC },
-		{ KC_DEL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_MINS, KC_EQL,
-				KC_LBRC, KC_RBRC, KC_BSLS }, { KC_LSFT, KC_F7, KC_F8, KC_F9,
-				KC_F10, KC_F11, KC_F12, KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN,
-				KC_ENT }, { KC_LCTRL, KC_LGUI, KC_LALT, DEFAULT, LOWER, KC_SPC,
-				KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT }
+			  {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC },
+			  {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS },
+			  {KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, KC_ENT  } ,
+			  {KC_LCTRL,KC_LGUI, KC_LALT, DEFAULT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT }
 
-};
+	};
 
 //	 uint16_t _DVORAK[MATRIX_ROWS][KEYMAP_COLS]={
 //
