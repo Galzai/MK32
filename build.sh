a6a45c3581b3d821660bac5135838059bf37396f
d.phan@ucl.ac.uk <d.phan@ucl.ac.uk>
2021-07-05 15:02:14 -0500

    CI/CD with Docker.

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/zsh
 
 # Build
-docker build -t ddp822/mk32 .
-docker push ddp822/mk32:latest
+docker build -t mk32:latest .
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/zsh
 
-docker run --rm -i -t --privileged ddp822/mk32:latest /bin/bash
+docker run --rm -i -t --privileged mk32:latest /bin/bash
