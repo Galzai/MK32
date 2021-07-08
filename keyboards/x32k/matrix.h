73ba0bfedf5e208a6c714d3f8ef2c8b7b3388636
d.phan@ucl.ac.uk <d.phan@ucl.ac.uk>
2021-07-08 02:02:35 -0500

    Adding default and CatiONE keyboards.

---
+++ keyboards/x32k/matrix.h
@@ -0,0 +1,49 @@
+/*
+ * matrix.h
+ *
+ *  Created on: 6 Oct 2018
+ *      Author: gal
+ */
+
+#ifndef MATRIX_H_
+#define MATRIX_H_
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <inttypes.h>
+#include "keyboard_config.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern uint8_t MATRIX_STATE[MATRIX_ROWS][MATRIX_COLS];
+extern uint8_t SLAVE_MATRIX_STATE[MATRIX_ROWS][MATRIX_COLS];
+extern uint8_t (*matrix_states[])[MATRIX_ROWS][MATRIX_COLS];
+
+/*
+ * @brief deinitialize rtc pins
+ */
+void rtc_matrix_deinit(void);
+
+/*
+ * @brief initialize rtc pins
+ */
+void rtc_matrix_setup(void);
+
+/*
+ * @brief initialize matrix
+ */
+void matrix_setup(void);
+
+/*
+ * @brief scan matrix
+ */
+void scan_matrix(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* MATRIX_H_ */
